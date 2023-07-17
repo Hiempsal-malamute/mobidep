15494eb51ea1d78559bba991e944f3e6c31299ed
Hiempsal-malamute <hass-box@hotmail.fr>
2023-07-17 16:30:15 +0200

    maj data 2020 + sum ipondi

--- data.py
+++ data.py
@@ -2,7 +2,10 @@ import pandas as pd
 import geopandas as gpd 
 import dask.dataframe as dd
 
-migdep_init = dd.read_csv("data/raw/FD_MIGGCO_2019.csv",sep=";",dtype={"COMMUNE":str,"IPONDI":float,"DRAN":str})
+migdep_init = dd.read_csv("data/raw/FD_MIGGCO_2020.csv",sep=";",dtype={"COMMUNE":str,"IPONDI":float,"DRAN":str})
+# len(migdep_init)
+
+# XY 
 ctr_dep = gpd.read_file("data/raw/fr-drom_ctr_dep2020_2154_style-1_gen.gpkg")[["insee_dep","geometry"]]
 
 ctr_dep["geometry"] = ctr_dep.geometry.to_crs(4326)
@@ -19,7 +22,7 @@ migdep["DRACT"] = migdep["COMMUNE"].str.slice(0,2)
 migdep = migdep[["DRACT","DRAN","IPONDI"]]
 
 # agrégation et comptage du nb de ménages ayant déménagé par departement de résidence actuel
-count_init = migdep.groupby(["DRAN","DRACT"]).count().reset_index().compute()
+count_init = migdep.groupby(["DRAN","DRACT"])["IPONDI"].sum().reset_index().compute()
 
 # conversion code dep en caractère
 count = count_init.astype({"DRAN":"string","DRACT":"string","IPONDI":"float"})
@@ -36,6 +39,5 @@ new1 = new.merge(ctr_dran,"left",left_on="DRAN",right_on="insee_dep")
 
 # filtre pour supprimer les mobilités internes
 new1 = new1[new1["DRACT"] != new1["DRAN"]]
-new1
 
-new1.to_csv("data/mig-res-2019.csv",index=False)
+new1.to_csv("data/mig-res-2020.csv",index=False)
